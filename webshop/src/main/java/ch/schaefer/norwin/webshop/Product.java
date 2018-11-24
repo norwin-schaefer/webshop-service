6f5a300f8e2cd69ffaf8cc9edf747f79f76884dc
Martin <Martin@DESKTOP-R8J1KC7>
2018-11-24 16:30:35 +0100

    Added Product Class

---
+++ webshop/src/main/java/ch/schaefer/norwin/webshop/Product.java
@@ -0,0 +1,42 @@
+package ch.schaefer.norwin.webshop;
+
+public class Product {
+
+	private String name;
+	private String description;
+	private float price;
+
+	public Product() {
+	}
+
+	public Product(String name, String description, float price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
