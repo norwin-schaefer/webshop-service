0dbd7194f2bceee15f63c887be041fc15cb059fd
Martin <Martin@DESKTOP-R8J1KC7>
2018-11-24 20:18:00 +0100

    added swagger UI

--- webshop/src/main/java/ch/schaefer/norwin/webshop/WebShopApplication.java
+++ webshop/src/main/java/ch/schaefer/norwin/webshop/WebShopApplication.java
@@ -3,11 +3,14 @@ package ch.schaefer.norwin.webshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class WebShopApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebShopApplication.class, args);
-    }
-	
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
 }
--- webshop/src/main/java/ch/schaefer/norwin/webshop/WebShopController.java
+++ webshop/src/main/java/ch/schaefer/norwin/webshop/WebShopController.java
@@ -2,14 +2,17 @@ package ch.schaefer.norwin.webshop;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class WebShopController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping(method = RequestMethod.GET, path = "/greeting")
 	public Greeting greeting(@RequestParam("message") String message) {
 		Greeting greeting = new Greeting();
 		greeting.setMessage(message);
